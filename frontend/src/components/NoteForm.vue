5d9ea76de37b7cd60b81dc3000241198238acfaa
Sebastian Solano <sebaxsolanom@gmail.com>
2024-09-30 16:58:31 -0500

    General menu and NoteForm

---
+++ frontend/src/components/NoteForm.vue
@@ -0,0 +1,34 @@
+<script setup></script>
+<template>
+  <form @submit.prevent="" class="">
+    <div>
+      <label class="block text-gray-700">Title</label>
+      <a-input v-model:value="userName" placeholder="Basic usage">
+        <template #prefix>
+          <user-outlined />
+        </template>
+        <template #suffix>
+          <a-tooltip title="Extra information">
+            <info-circle-outlined style="color: rgba(0, 0, 0, 0.45)" />
+          </a-tooltip>
+        </template>
+      </a-input>
+    </div>
+    <div>
+      <label class="block text-gray-700">Description</label>
+      <a-textarea
+        v-model:value="value2"
+        placeholder="Autosize height with minimum and maximum number of lines"
+        :auto-size="{ minRows: 2, maxRows: 5 }"
+      />
+    </div>
+    <div>
+      <label class="block text-gray-700">Title</label>
+      <input type="text" class="w-full p-2 border border-gray-300 rounded" />
+    </div>
+    <div>
+      <label class="block text-gray-700">Title</label>
+      <input type="text" class="w-full p-2 border border-gray-300 rounded" />
+    </div>
+  </form>
+</template>
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,9 +1,7 @@
-import Vue from "vue";
-import Vuex from "vuex";
+// src/store.js
+import { createStore } from "vuex";
 
-Vue.use(Vuex);
-
-export default new Vuex.Store({
+export default createStore({
   state: {
     notes: {
       active: [],

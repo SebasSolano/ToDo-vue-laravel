2f6a936634de55b989418979b858fce74307abfd
Sebastian Solano <sebaxsolanom@gmail.com>
2024-09-30 23:46:48 -0500

    auth completed

--- frontend/src/components/auth/Register.vue
+++ frontend/src/components/auth/Register.vue
@@ -1,2 +1,109 @@
-<script setup></script>
-<template></template>
+<script setup>
+  import { ref, reactive } from "vue";
+  import { useRouter } from "vue-router";
+  import { message } from "ant-design-vue";
+
+  const router = useRouter();
+  const isLoading = ref(false);
+  const form = reactive({
+    name: "",
+    email: "",
+    password: "",
+    confPassword: "",
+  });
+
+  async function onSubmit(event) {
+    event.preventDefault();
+    isLoading.value = true;
+    try {
+      if (form.password !== form.confPassword) {
+        message.error("Passwords do not match");
+        isLoading.value = false;
+        return;
+      }
+      message.success("You have registered successfully!");
+      setTimeout(() => {
+        isLoading.value = false;
+        router.push("/auth");
+      }, 1000);
+    } catch (error) {
+      setTimeout(() => {
+        isLoading.value = false;
+      }, 1000);
+    }
+  }
+</script>
+
+<template>
+  <div class="grid gap-6">
+    <form @submit="onSubmit">
+      <div class="grid gap-2">
+        <div class="grid gap-1">
+          <label for="email"> Name </label>
+          <a-input
+            v-model="form.name"
+            id="name"
+            placeholder="Name"
+            type="name"
+            auto-capitalize="none"
+            auto-complete="name"
+            auto-correct="off"
+            :disabled="isLoading"
+            required
+          />
+          <label for="email"> Email </label>
+          <a-input
+            v-model="form.email"
+            id="email"
+            placeholder="Email"
+            type="email"
+            auto-capitalize="none"
+            auto-complete="email"
+            auto-correct="off"
+            :disabled="isLoading"
+            required
+          />
+          <label for="password"> Password </label>
+          <a-input-password
+            v-model="form.password"
+            id="password"
+            placeholder="Password"
+            type="password"
+            auto-capitalize="none"
+            auto-complete="password"
+            auto-correct="off"
+            :disabled="isLoading"
+            required
+          />
+        </div>
+        <div>
+          <label for="password"> Confirm Password </label>
+          <a-input-password
+            v-model="form.confPassword"
+            id="confPassword"
+            placeholder="Confirm Password"
+            type="password"
+            auto-capitalize="none"
+            auto-complete="password"
+            auto-correct="off"
+            :disabled="isLoading"
+            required
+          />
+        </div>
+        <a-button
+          html-type="submit"
+          type="primary"
+          :loading="isLoading"
+          :disabled="isLoading"
+        >
+          Sing in
+        </a-button>
+      </div>
+    </form>
+    <div class="relative">
+      <div class="absolute inset-0 flex items-center">
+        <span class="w-full border-t" />
+      </div>
+    </div>
+  </div>
+</template>
